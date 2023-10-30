ca02d73a4a3218d829bfbcc9363859f46ec876a8
Tianyu Li <tianyuli@meta.com>
2023-10-30 09:54:33 -0700

    [autojump] Hack to fix autojump not found issue in oh my zsh

--- plugins/autojump/autojump.plugin.zsh
+++ plugins/autojump/autojump.plugin.zsh
@@ -13,6 +13,7 @@ autojump_paths=(
   /usr/local/etc/profile.d/autojump.sh                   # macOS with Homebrew (default)
   /opt/homebrew/etc/profile.d/autojump.sh                # macOS with Homebrew (default on M1 macs)
   /etc/profiles/per-user/$USER/etc/profile.d/autojump.sh # macOS Nix, Home Manager and flakes
+  $HOME/homebrew/share/autojump/autojump.zsh             # Hack to fix the "autojump not install issue"
 )
 
 for file in $autojump_paths; do
